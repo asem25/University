19361ede0719c227e69d7c1d0f02326b037a624b
asem25 <161457425+asem25@users.noreply.github.com>
2024-04-06 00:17:08 +0300

    Main.java

--- laboratory work/OOP/Main.java
+++ laboratory work/OOP/Main.java
@@ -1,14 +1,11 @@
 
 
 import java.io.*;
-import java.math.BigInteger;
-import java.security.SecureRandom;
-import java.text.DecimalFormat;
-import java.text.MessageFormat;
 import java.util.*;
 
 
 public class homework {
+    //объявление шаблонов
     private static final String[] units = { "", "один", "два", "три", "четыре",
             "пять", "шесть", "семь", "восемь", "девять", "десять",
             "одиннадцать", "двенадцать", "тринадцать", "четырнадцать",
@@ -38,53 +35,45 @@ public class homework {
             "восемьдесят", // 8
             "девяносто"   // 9
     };
+    // Исполняющий метод 
     public static void main(String[] args) throws IOException {
+        // Объявление объекта, который читает из консоли
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true) {
-            String frombuf =reader.readLine();
-            if (frombuf.equals("exit")){
-                break;
-            }
+        // чтение строки из консоли
+        String frombuf = reader.readLine();
 
-            if (!frombuf.isEmpty()) {
+        // проверка на пустую строку
+        if (!frombuf.isEmpty()) {
+                //приведение к int
                 Integer intFromConsole = Integer.parseInt(frombuf);
-                if (intFromConsole < 10000){
+                
+            if (intFromConsole < 10000){
                     logger(intFromConsole);
-                }else
-                    System.out.println(convert(intFromConsole));
-            }
-
+            }else
+                System.out.println(convert(intFromConsole));
         }
-
-
-
-
-
-
     }
     private static void logger(int numb){
         System.out.println(String.format("Было введено %d-значное число", String.valueOf(numb).length()));
     }
-
+    //Метод обработки
     public static String convert(int number) {
-
+        // для упрощения реализации использовал рекурсию
+        // использовал тернарный оператор, чтобы избежать огромного кол-ов if-else
+        // формула для тернарного оператора : bool expression ?  true : false
         if (number < 0) {
             return "минус " + convert(-number);
         }
-
         if (number < 20) {
             return units[number];
         }
-
         if (number < 100) {
             return tens[number / 10] + ((number % 10 != 0) ? " " + units[number % 10] : "");
         }
-
         if (number < 1000) {
             return hundreds[number / 100] + ((number % 100 != 0) ? " " + convert(number % 100) : "");
         }
-
         if (number < 10000) {
             String thousandsPart = (number / 1000 == 1 && number % 1000 != 0) ? "одна тысяча" :
                     (number / 1000 == 2) ? "две тысячи" :
@@ -93,11 +82,10 @@ public class homework {
             String rest = number % 1000 != 0 ? " " + convert(number % 1000) : "";
             return thousandsPart + rest;
         }
-
         if (number < 100000) {
             if (number % 10000 != 0) {
                 int i = 1 + (number / 1000) % 10 == 1 ? 1 : 1 + (number / 1000) % 10 == 2 ? 2
-                        : ((number / 1000) % 10 > 2) & ((number / 1000) % 10) <= 4 ? 2 : 3; // формула для тернарного оператора : bool exp ?  true : false
+                        : ((number / 1000) % 10 > 2) & ((number / 1000) % 10) <= 4 ? 2 : 3; 
 
                 String thousandsPart = convert(number / 1000) + " " + thousands[i];
                 thousandsPart = thousandsPart.replaceAll("два тысяч", "две тыcячи");
@@ -108,7 +96,6 @@ public class homework {
                 return convert(number / 1000) + " " + thousands[3];
             }
         }
-
         return "number < 100000 is false";
     }
 
